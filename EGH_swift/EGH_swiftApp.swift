f7d12a50c768f9373d843e4f87ecbc7f609a3d80
takaaaaaayuki <s2322045@stu.musashino-u.ac.jp>
2025-02-24 22:26:38 +0900

    Initial Commit

---
+++ EGH_swift/EGH_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  EGH_swiftApp.swift
+//  EGH_swift
+//
+//  Created by takayuki chisyaki on 2025/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EGH_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EGH_swiftTests/EGH_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  EGH_swiftTests.swift
+//  EGH_swiftTests
+//
+//  Created by takayuki chisyaki on 2025/02/24.
+//
+
+import Testing
+@testable import EGH_swift
+
+struct EGH_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
